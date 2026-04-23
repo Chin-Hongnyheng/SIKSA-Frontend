4b3bb88f0f2ff9041612c67f69a7de4cc17835a1
phirum <sophaphirum2@gmail.com>
2026-04-23 15:25:58 +0700

    QR scan page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'core/theme/app_theme.dart';
+import 'Attendance/studentAttendance.dart';
+import 'Attendance/teacherAttendance.dart';
+import 'qrcode/scan_code_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,25 +17,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.lightTheme,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('SIKSA APP', style: GoogleFonts.nunito(fontSize: 20)),
-          backgroundColor: Colors.green,
-          foregroundColor: Colors.white,
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Text(
-                'Hello, Welcome to SIKSA APP!',
-                style: TextStyle(fontSize: 30),
-              ),
-              Text('SIKSA APP', style: GoogleFonts.nunito(fontSize: 20)),
-            ],
-          ),
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
+      // home: const StudentAttendance(), 
+      home: ScanCodePage(),
+      debugShowCheckedModeBanner: false
     );
   }
 }
